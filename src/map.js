a0e6722ac96c23320097fae939fcde9e564a12b8
Lucas Rollet <lucas.rollet.69@gmail.com>
2022-05-31 00:39:25 +0200

    Finish map

--- src/map.js
+++ src/map.js
@@ -1,208 +1,215 @@
-class MapPlot {
-  makeColorbar(
-    svg,
-    color_scale,
-    top_left,
-    colorbar_size,
-    scaleClass = d3.scaleLog
-  ) {
-    const value_to_svg = scaleClass()
-      .domain(color_scale.domain())
-      .range([colorbar_size[1], 0]);
+/**
+ * Handles the main map component using Leaflet.
+ * The data has been parsed previously and encoded as a GeoJson object.
+ */
 
-    const range01_to_color = d3
-      .scaleLinear()
-      .domain([0, 1])
-      .range(color_scale.range())
-      .interpolate(color_scale.interpolate());
+var map = L.map("map").setView([0, 0], 2);
 
-    // Axis numbers
-    const colorbar_axis = d3
-      .axisLeft(value_to_svg)
-      .tickFormat(d3.format(".0f"));
+/**
+ * Map settings
+ */
+const PALETTE = [
+  "#FFFFFF",
+  "#FFFFB2",
+  "#FECC5C",
+  "#FD8D3C",
+  "#F03B20",
+  "#BD0026",
+];
 
-    const colorbar_g = this.svg
-      .append("g")
-      .attr("id", "colorbar")
-      .attr("transform", "translate(" + top_left[0] + ", " + top_left[1] + ")")
-      .call(colorbar_axis);
+const AVAILABLE_MODES = [
+  {
+    data_key: "prop_food",
+    label: "Food: Aliment for feeding people",
+    grades: [300, 500, 15000, 50000],
+    unit: "thousand tons/capita",
+  },
+  {
+    data_key: "prop_feed",
+    label: "Feed: Aliment for feeding livestock",
+    grades: [300, 500, 15000, 50000],
+    unit: "thousand tons/capita",
+  },
+  {
+    data_key: "food_supply",
+    label: "Overall: overall aliment supply",
+    grades: [2300, 2600, 3100, 3400],
+    unit: "kcal/capita/day",
+  },
+];
 
-    // Create the gradient
-    function range01(steps) {
-      return Array.from(Array(steps), (elem, index) => index / (steps - 1));
-    }
+/**
+ * Event listeners
+ */
+// The display mode and the current year displayed can be changed dynamically
+const map_state = {
+  mode: AVAILABLE_MODES[2],
+  year: 51,
+};
 
-    const svg_defs = this.svg.append("defs");
-
-    const gradient = svg_defs
-      .append("linearGradient")
-      .attr("id", "colorbar-gradient")
-      .attr("x1", "0%") // bottom
-      .attr("y1", "100%")
-      .attr("x2", "0%") // to top
-      .attr("y2", "0%")
-      .attr("spreadMethod", "pad");
-
-    gradient
-      .selectAll("stop")
-      .data(range01(10))
-      .enter()
-      .append("stop")
-      .attr("offset", (d) => Math.round(100 * d) + "%")
-      .attr("stop-color", (d) => range01_to_color(d))
-      .attr("stop-opacity", 1);
-
-    // create the colorful rect
-    colorbar_g
-      .append("rect")
-      .attr("id", "colorbar-area")
-      .attr("width", colorbar_size[0])
-      .attr("height", colorbar_size[1])
-      .style("fill", "url(#colorbar-gradient)")
-      .style("stroke", "black")
-      .style("stroke-width", "1px");
-  }
-
-  constructor(svg_element_id) {
-    this.svg = d3.select("#" + svg_element_id);
-
-    // may be useful for calculating scales
-    const svg_viewbox = this.svg.node().viewBox.animVal;
-    this.svg_width = svg_viewbox.width;
-    this.svg_height = svg_viewbox.height;
-
-    // D3 Projection
-    // similar to scales
-    const projection = d3
-      .geoNaturalEarth1()
-      .rotate([0, 0])
-      .center([8.3, 46.8]) // WorldSpace: Latitude and longitude of center of switzerland
-      .scale(13000)
-      .translate([this.svg_width / 2, this.svg_height / 2]) // SVG space
-      .precision(0.1);
-
-    // path generator to convert JSON to SVG paths
-    const path_generator = d3.geoPath().projection(projection);
-
-    //colormap for population density
-    const color_scale = d3
-      .scaleLog()
-      .range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
-      .interpolate(d3.interpolateHcl);
-
-    const population_promise = d3
-      .csv("http://161.35.216.145/assets/data/cantons-population.csv")
-      .then((data) => {
-        let cantonId_to_population = {};
-        data.forEach((row) => {
-          cantonId_to_population[row.code] = parseFloat(row.density);
-        });
-        return cantonId_to_population;
-      });
-
-    const map_promise = d3
-      .json("http://161.35.216.145/assets/data/ch-cantons.json")
-      .then((topojson_raw) => {
-        const canton_paths = topojson.feature(
-          topojson_raw,
-          topojson_raw.objects.cantons
-        );
-        return canton_paths.features;
-      });
-
-    const point_promise = d3
-      .csv("http://161.35.216.145/assets/data/locations.csv")
-      .then((data) => {
-        let new_data = [];
-
-        for (let idx = 0; idx < data.length; idx += 10) {
-          new_data.push(data[idx]);
-        }
-
-        return new_data;
-      });
-
-    Promise.all([population_promise, map_promise, point_promise]).then(
-      (results) => {
-        let cantonId_to_population = results[0];
-        let map_data = results[1];
-        let point_data = results[2];
-
-        map_data.forEach((canton) => {
-          canton.properties.density = cantonId_to_population[canton.id];
-        });
-
-        const densities = Object.values(cantonId_to_population);
-
-        // color_scale.domain([d3.quantile(densities, .01), d3.quantile(densities, .99)]);
-        color_scale.domain([d3.min(densities), d3.max(densities)]);
-
-        // Order of creating groups decides what is on top
-        this.map_container = this.svg.append("g");
-        this.point_container = this.svg.append("g");
-        this.label_container = this.svg.append("g"); // <- is on top
-
-        //color the map according to the density of each canton
-        this.map_container
-          .selectAll(".canton")
-          .data(map_data)
-          .enter()
-          .append("path")
-          .classed("canton", true)
-          .attr("d", path_generator)
-          .style("fill", (d) => color_scale(d.properties.density));
-
-        this.label_container
-          .selectAll(".canton-label")
-          .data(map_data)
-          .enter()
-          .append("text")
-          .classed("canton-label", true)
-          .attr(
-            "transform",
-            (d) => "translate(" + path_generator.centroid(d) + ")"
-          )
-          //.translate((d) => path_generator.centroid(d))
-          .attr("dy", ".35em")
-          .text((d) => d.properties.name);
-
-        const r = 3;
-
-        this.point_container
-          .selectAll(".point")
-          .data(point_data)
-          .enter()
-          .append("circle")
-          .classed("point", true)
-          .attr("r", r)
-          .attr("cx", -r)
-          .attr("cy", -r)
-          .attr(
-            "transform",
-            (d) => "translate(" + projection([d.lon, d.lat]) + ")"
-          );
-
-        this.makeColorbar(
-          this.svg,
-          color_scale,
-          [50, 30],
-          [20, this.svg_height - 2 * 30]
-        );
-      }
-    );
-  }
-}
-
-function whenDocumentLoaded(action) {
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", action);
-  } else {
-    // `DOMContentLoaded` already fired
-    action();
-  }
-}
-
-whenDocumentLoaded(() => {
-  plot_object = new MapPlot("map-plot");
-  // plot object is global, you can inspect it in the dev-console
+// Update map display mode
+document.getElementsByName("scales").forEach((el) => {
+  el.addEventListener("change", function () {
+    map_state.mode = AVAILABLE_MODES[parseInt(this.value)];
+    reset_geojson();
+  });
 });
+
+// Handle year slider events. Update the map and label on input
+let slider = document.getElementById("current_year_slider");
+slider.addEventListener("input", function () {
+  map_state.year = this.value;
+  document.getElementById("current_year").innerHTML =
+    1961 + parseInt(this.value);
+  reset_geojson();
+});
+
+/**
+ * Map UTILS functions
+ */
+
+// Returns the color for a given cell
+function getColor(d, feature) {
+  let thresholds = map_state.mode.grades;
+
+  return d === 0
+    ? PALETTE[0]
+    : d > thresholds[3]
+    ? PALETTE[5]
+    : d > thresholds[2]
+    ? PALETTE[4]
+    : d > thresholds[1]
+    ? PALETTE[3]
+    : d > thresholds[0]
+    ? PALETTE[2]
+    : d < thresholds[0]
+    ? PALETTE[1]
+    : PALETTE[0];
+}
+
+// Style for each country
+const style = (feature) => {
+  const property = feature.properties[map_state.mode.data_key];
+  return {
+    fillColor: getColor(property ? property[map_state.year] : 0, "food_supply"),
+    weight: 1,
+    opacity: 1,
+    color: "white",
+    dashArray: "1",
+    fillOpacity: 0.7,
+  };
+};
+
+/**
+ * Map Setup
+ */
+var geojson, info, legend;
+
+// On country hover, change the border style.
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+    weight: 2,
+    color: "#666",
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+// On country unhover, reset
+function resetHighlight(e) {
+  geojson.resetStyle(e.target);
+  info.update();
+}
+
+// On country click, zoom to it
+function zoomToFeature(e) {
+  map.fitBounds(e.target.getBounds());
+}
+
+// Setup the event listeners for each feature
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature,
+  });
+}
+
+// Called every time the map needs to be redrawn with new data
+function reset_geojson() {
+  if (geojson) map.removeLayer(geojson);
+
+  geojson = L.geoJson(worldData, { style, onEachFeature }).addTo(map);
+
+  if (info) info.update();
+  if (legend) legend.addTo(map);
+}
+
+var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  className: "map-tiles",
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+reset_geojson();
+
+/**
+ * TOP RIGHT INFO CONTROL
+ * Displays the hovered state's value
+ */
+info = L.control();
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create("div", "info");
+  this.update();
+
+  return this._div;
+};
+
+info.update = function (props) {
+  console.log(props);
+  let value =
+    props && props[map_state.mode.data_key]
+      ? Math.round(props[map_state.mode.data_key][map_state.year])
+      : 0;
+
+  this._div.innerHTML = `
+    <h4>${map_state.mode.label}</h4>
+    ${
+      props
+        ? `<b>${props.NAME_LONG}</b><br/>${value} ${map_state.mode.unit}`
+        : "Hover over a state"
+    }
+  `;
+};
+info.addTo(map);
+
+/**
+ * BOTTOM RIGHT LEGEND CONTROL
+ * Displays the current grades scale
+ */
+legend = L.control({ position: "bottomright" });
+legend.onAdd = function (map) {
+  let div = L.DomUtil.create("div", "info legend");
+  let grades = map_state.mode.grades;
+
+  // Colored square for each grade
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML += `
+      <div>
+        <i style="background: ${getColor(grades[i] + 1)}"></i>
+        ${grades[i]}${grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br/>" : "+"}
+      </div>
+    `;
+  }
+
+  return div;
+};
+
+legend.addTo(map);
