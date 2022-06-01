70efbca30022412750ec33c911939e558b740152
meghanharrington <mharrin9@uvm.edu>
2022-06-01 12:58:27 +0200

    updates

--- src/map.js
+++ src/map.js
@@ -21,14 +21,14 @@ const AVAILABLE_MODES = [
   {
     data_key: "prop_food",
     label: "Food: Aliment for feeding people",
-    grades: [300, 500, 15000, 50000],
-    unit: "thousand tons/capita",
+    grades: [800, 1200, 1400, 1800],
+    unit: "tons/capita",
   },
   {
     data_key: "prop_feed",
     label: "Feed: Aliment for feeding livestock",
-    grades: [300, 500, 15000, 50000],
-    unit: "thousand tons/capita",
+    grades: [200, 400, 800, 1000],
+    unit: "tons/capita",
   },
   {
     data_key: "food_supply",
@@ -44,7 +44,7 @@ const AVAILABLE_MODES = [
 // The display mode and the current year displayed can be changed dynamically
 const map_state = {
   mode: AVAILABLE_MODES[2],
-  year: 52,
+  year: 57,
 };
 
 // Update map display mode
@@ -213,3 +213,199 @@ legend.onAdd = function (map) {
 };
 
 legend.addTo(map);
+
+
+
+
+
+// Pie chart
+
+var width = 150
+    height = 150
+    margin = 20;
+
+var radius = Math.min(width, height) / 2 - margin;
+
+var svg = d3.select("#piechart")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+// Read the data
+d3.csv("data/pie.csv", read, function(error, data) {
+
+// TODO: connect to map timeline
+  var year = "1961";
+  //var currentData = data.filter(function(d) {return d["Year"] == (1961 + parseInt(map_state.year)).toString()});
+  var currentData = data.filter(function(d) {return d["Year"] == year});
+
+// draw the pie chart with the data from the chosen year
+ update(currentData);
+
+
+ function update(data) {
+   // draw the legend
+   var feedprop = data.filter( function(d) {return d.Element == 'Feed'})[0].Prop;
+   var foodprop = data.filter( function(d) {return d.Element == 'Food'})[0].Prop;
+
+   d3.select('#pielegend')
+       .text("Food: " + parseFloat(foodprop*100).toFixed(2)+"%" + "\nFeed: " + parseFloat(feedprop*100).toFixed(2)+"%");
+
+    // create the pie chart
+    var pie = d3.layout.pie()
+      .value(function(d) {return d.value.Prop; })
+      .sort(function(a, b) { return d3.ascending(a.key, b.key);} )
+    var data_pie = pie(d3.entries(data))
+
+    // draw the paths
+    var u = svg.selectAll("path")
+      .data(data_pie)
+
+    u.enter()
+      .append('path')
+      .attr('d', d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(radius))
+      .attr('fill', function(d){
+        if (d.data.value.Element == "Feed") {
+          return "red"
+        }
+        else return "orange"
+     })
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 1)
+
+    u.exit()
+      .remove()
+
+
+  }
+
+
+
+});
+
+// read in data
+function read(d) {
+  d.year = d.Year;
+  d.cat = d.Element;
+  d.prop = +d.prop;
+  return d;
+}
+
+
+
+
+
+
+// Calorie Bars
+
+d3.csv("data/foodsupply.csv", function(d) {
+
+  var dictionary = {};
+
+  for (var i = 0, entry; i < d.length; i++) {
+    entry = d[i];
+    dictionary[entry.Year.concat(entry.Continent)] =
+      entry.Value;
+  }
+
+  var max = 2500;
+  var year = "1961";
+
+  update(year);
+
+  function update(year) {
+
+
+    // max calories and calories for the given year
+    var asia_data = [2500, dictionary[year.concat("Asia")]];
+    var africa_data = [2500, dictionary[year.concat("Africa")]];
+    var eur_data = [2500, dictionary[year.concat("Europe")]];
+    var na_data = [2500, dictionary[year.concat("North America")]];
+    var sa_data = [2500, dictionary[year.concat("South America")]];
+    var o_data = [2500, dictionary[year.concat("Oceania")]];
+
+
+    // create svgs for each continent
+    var asia_bar = d3.select("#asia", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var europe_bar = d3.select("#eur", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var oceania_bar = d3.select("#oce", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var na_bar = d3.select("#northam", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var sa_bar = d3.select("#southam", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var africa_bar = d3.select("#afr", ".supply-stickman").append("svg")
+     .attr("class", "chart")
+     .attr("width", 200)
+     .attr("height", 40);
+
+    var x = d3.scale.linear()
+     .domain([0, max])
+     .range([0, 200]);
+
+
+     // create two bars for each continent
+    asia_bar.selectAll("rect")
+     .data(asia_data)
+    .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+
+
+   africa_bar.selectAll("rect")
+     .data(africa_data)
+   .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+
+
+   europe_bar.selectAll("rect")
+     .data(eur_data)
+   .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+
+   na_bar.selectAll("rect")
+     .data(na_data)
+   .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+
+   sa_bar.selectAll("rect")
+     .data(sa_data)
+   .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+
+
+   oceania_bar.selectAll("rect")
+     .data(o_data)
+   .enter().append("rect")
+     .attr("width", x)
+     .attr("height", 20);
+}
+
+    });
